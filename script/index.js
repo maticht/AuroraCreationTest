3cf070c698d40fdf37a8723860a592f3c69ff37f
maticht <74956969+maticht@users.noreply.github.com>
2022-08-17 00:10:08 +0200

    5
    5

--- script/index.js
+++ script/index.js
@@ -1,7 +1,7 @@
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.menu');
 
-menuBtn.addEventListener('click', function(){
+menuBtn.addEventListener('click', () => {
     menuBtn.classList.toggle('active');
     menu.classList.toggle('active');
 })
